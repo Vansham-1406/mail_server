258614b097dc43d4afce188e1846d3792332540c
Vansham-1406 <vanshamaggarwal6@gmail.com>
2023-01-24 00:16:58 +0530

    update

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,16 +1,143 @@
-import React from 'react'
-import MenuIcon from '@mui/icons-material/Menu';
-
+import React, { useState, useEffect } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import './App.css';
+import "./App.css";
+
 const Main = () => {
+  const [nav, setNav] = useState(true);
+  const navigate = useNavigate();
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("userData"));
+    if (items) {
+      setItems(items);
+    }
+  }, []);
 
   return (
     <div>
-      {/* {JSON.parse(localStorage.getItem("userData"))?.firstName || ""} */}
-      
+      {items?.firstName ? (
+        <div>
+          <div className={nav ? "mainNav" : "mainNav1"}>
+            {nav ? (
+              <div className="d-flex justify-content-between">
+                <MenuIcon
+                  className="fs-2 text-light ms-4 mt-3 mb-3 hamburger"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    setNav(false);
+                  }}
+                />
+                <h4 className="pt-3" style={{ color: "white" }}>
+                  Welcome,{" "}
+                  <span className="text-capitalize">{items?.firstName} </span>
+                  <span className="text-capitalize">{items?.lastName} </span>
+                </h4>
+                <Button
+                  variant="contained"
+                  color="success"
+                  style={{ width: "80px", height: "40px", marginTop: "12px" }}
+                  className="me-3"
+                  onClick={() => {
+                    navigate("/");
+                    localStorage.removeItem("userData");
+                    setItems([]);
+                  }}
+                >
+                  Logout
+                </Button>
+              </div>
+            ) : (
+              <div className="d-flex justify-content-between">
+                <MenuOpenIcon
+                  className="fs-2 mt-3 mb-3 menuPos"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    setNav(true);
+                  }}
+                />
+                <h4 className="pt-3" style={{ color: "white" }}>
+                  Welcome,{" "}
+                  {JSON.parse(localStorage.getItem("userData"))?.firstName ||
+                    ""}{" "}
+                  {JSON.parse(localStorage.getItem("userData"))?.lastName || ""}
+                </h4>
+                <Button
+                  variant="contained"
+                  color="success"
+                  style={{ width: "80px", height: "40px", marginTop: "12px" }}
+                  className="me-3"
+                  onClick={() => {
+                    localStorage.removeItem("userData");
+                    navigate("/");
+                  }}
+                >
+                  Logout
+                </Button>
+              </div>
+            )}
+          </div>
+          <div className="d-flex">
+            <div className={nav ? "navbar" : "navbar1"}>
+              <div className="d-flex flex-column">
+                <div className="mt-5 d-flex navbar_hover cursor_pointer">
+                  <HomeIcon className="fs-2 icon_color ms-3 me-4 mt-3 mb-3"/>
+                  {!nav && <p className="fs-6 fw-bold marg">Dashboard</p>}
+                </div>
+                <div className="d-flex navbar_hover cursor_pointer">
+                  <StarRateIcon className="fs-2 icon_color ms-3 me-4 mt-3 mb-3" />
+                  {!nav && <p className="fs-6 fw-bold marg">Saved</p>}
+                </div>
+                <div className="d-flex navbar_hover cursor_pointer">
+                  <DeleteIcon className="fs-2 icon_color ms-3 me-4 mt-3 mb-3" />
+                  {!nav && <p className="fs-6 fw-bold marg">Deleted</p>}
+                </div>
+                <div className="d-flex navbar_hover cursor_pointer">
+                  <AccountBoxIcon className="fs-2 icon_color ms-3 me-4 mt-3 mb-3" />
+                  {!nav && <p className="fs-6 fw-bold marg">Profile</p>}
+                </div>
+              </div>
+            </div>
+            <div className={nav ? "dashboard" : "dashboard1"}></div>
+          </div>
+        </div>
+      ) : (
+        <div className="main_banner">
+          <div className="position-absolute top-50 start-50 translate-middle d-flex flex-column text-dark text-center">
+            <p className="fs-2 fw-bold">Welcome to Mail Server!</p>
+            <p className="fs-2">Login/Sigup to send mails freely!</p>
+            <div className="d-flex justify-content-center mt-2">
+              <button
+                className="me-3 btn btn-secondary ps-3 pe-3"
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                Login
+              </button>
+              <button
+                className="ms-3 btn btn-secondary ps-3 pe-3"
+                onClick={() => {
+                  navigate("/signup");
+                }}
+              >
+                Signup
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,159 +1,171 @@
-import React from 'react'
-// PARTICLE JS START 
+import React from "react";
+// PARTICLE JS START
 import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-// PARTICLE JS END 
+// PARTICLE JS END
 
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import './App.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
 
-    const particlesInit = useCallback(async engine => {
-        console.log(engine);
-        await loadFull(engine);
-    }, []);
+  const particlesLoaded = useCallback(async (container) => {
+  }, []);
 
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-    }, []);
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div className='home_banner position-relative'>
-        <>
+    <div className="home_banner position-relative">
+      <>
         <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "#1f263b",
-                    },
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#1f263b",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: false,
+                  mode: "bubble",
                 },
-                fpsLimit: 120,
-                interactivity: {
-                    detect_on: "canvas",
-                    events: {
-                      onhover: {
-                        enable: false,
-                        mode: "bubble"
-                      },
-                      onclick: {
-                        enable: true,
-                        mode: "repulse"
-                      },
-                      resize: true
-                    },
-                    modes: {
-                      grab: {
-                        distance: 400,
-                        line_linked: {
-                          opacity: 0.5
-                        }
-                      },
-                      bubble: {
-                        distance: 400,
-                        size: 4,
-                        duration: 0.3,
-                        opacity: 1,
-                        speed: 3
-                      },
-                      repulse: {
-                        distance: 200,
-                        duration: 0.4
-                      },
-                      push: {
-                        particles_nb: 4
-                      },
-                      remove: {
-                        particles_nb: 2
-                      }
-                    }
+                onclick: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 400,
+                  line_linked: {
+                    opacity: 0.5,
                   },
-                particles:{
-                    number: {
-                      value: 52,
-                      density: {
-                        enable: true,
-                        value_area: 631.3280775270874
-                      }
-                    },
-                    color: {
-                      value: "#fff"
-                    },
-                    shape: {
-                      type: "triangle",
-                      stroke: {
-                        width: 0,
-                        color: "#000000"
-                      }
-                    },
-                    opacity: {
-                      value: 0.5,
-                      random: true,
-                      anim: {
-                        enable: false,
-                        speed: 1,
-                        opacity_min: 0.1,
-                        sync: false
-                      }
-                    },
-                    size: {
-                      value: 5,
-                      random: true,
-                      anim: {
-                        enable: false,
-                        speed: 40,
-                        size_min: 0.1,
-                        sync: false
-                      }
-                    },
-                    line_linked: {
-                      enable: false,
-                      distance: 500,
-                      color: "#ffffff",
-                      opacity: 0.4,
-                      width: 2
-                    },
-                    move: {
-                      enable: true,
-                      speed: 1.5,
-                      direction: "bottom",
-                      random: false,
-                      straight: false,
-                      out_mode: "out",
-                      bounce: false,
-                      attract: {
-                        enable: false,
-                        rotateX: 600,
-                        rotateY: 1200
-                      }
-                    }
-                  },
-                detectRetina: true,
-            }}
+                },
+                bubble: {
+                  distance: 400,
+                  size: 4,
+                  duration: 0.3,
+                  opacity: 1,
+                  speed: 3,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            particles: {
+              number: {
+                value: 52,
+                density: {
+                  enable: true,
+                  value_area: 631.3280775270874,
+                },
+              },
+              color: {
+                value: "#fff",
+              },
+              shape: {
+                type: "triangle",
+                stroke: {
+                  width: 0,
+                  color: "#000000",
+                },
+              },
+              opacity: {
+                value: 0.5,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 1,
+                  opacity_min: 0.1,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 5,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: false,
+                distance: 500,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 2,
+              },
+              move: {
+                enable: true,
+                speed: 1.5,
+                direction: "bottom",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200,
+                },
+              },
+            },
+            detectRetina: true,
+          }}
         />
-        </>
-          <img src='../logo.png' style={{height:"90px",width:"100px"}} className="d-flex justify-content-center align-items-center"/>
-        <div className='position-absolute top-50 start-50 translate-middle d-flex flex-column text-light text-center'>
-            <p className='fs-6 fw-bold'>Welcome to Mail Server!</p>
-            <p className='fs-6'>Login/Sigup to send mails freely!</p>
-            <div className='d-flex justify-content-center mt-2'>
-                <button className='me-3 btn btn-success ps-3 pe-3' onClick={()=>{
-                    navigate("/login")
-                }}>Login</button>
-                <button className='ms-3 btn btn-success ps-3 pe-3' onClick={()=>{
-                    navigate("/signup")
-                }}>Signup</button>
-            </div>
+      </>
+      <img
+        src="../logo.png"
+        style={{ height: "90px", width: "100px" }}
+        className="d-flex justify-content-center align-items-center"
+        alt="mailiefy logo"
+      />
+      <div className="position-absolute top-50 start-50 translate-middle d-flex flex-column text-light text-center">
+        <p className="fs-6 fw-bold">Welcome to Mail Server!</p>
+        <p className="fs-6">Login/Sigup to send mails freely!</p>
+        <div className="d-flex justify-content-center mt-2">
+          <button
+            className="me-3 btn btn-success ps-3 pe-3"
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            Login
+          </button>
+          <button
+            className="ms-3 btn btn-success ps-3 pe-3"
+            onClick={() => {
+              navigate("/signup");
+            }}
+          >
+            Signup
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
